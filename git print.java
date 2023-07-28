fc47f5a36d96dd74cacfd0dd3a978285a3a94d13
mahesh <140531490+Mahesh22598@users.noreply.github.com>
2023-07-28 11:41:36 +0530

    Add files via upload

---
+++ git print.java
@@ -0,0 +1,14 @@
+public class hellow {
+public static void main(String[] args) {
+// TODO Auto-generated method stub
+System.out.println("Hello Agni");
+System.out.println("Hello Riyhbbvva");
+  System.out.println("Hello mahesh");
+   System.out.println("Hello king22");
+   System.out.println("Hello hi kingini");
+  System.out.println("Hello mahhhi11");  
+  System.out.println("Hello mahhh555");  
+  System.out.println("Hello mahhh222");  
+  
+}
+}
